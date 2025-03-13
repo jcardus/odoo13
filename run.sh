b71e2675531fe198985b09d2b48b363b9a925dc7
jcardus <asklocation.net@gmail.com>
2025-03-13 04:11:09 +0000

    fleet

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-docker cp ./13.0 odoo:/usr/lib/python3/dist-packages
+docker cp ./13.0/odoo odoo:/usr/lib/python3/dist-packages
+curl -L https://github.com/OCA/fleet/archive/refs/heads/13.0.zip -o fleet.zip
+echo "Extracting the module"
+unzip fleet.zip
+mv -v fleet-13.0/* addons
+rm -rf  fleet-13.0 fleet.zip || true
+docker-compose restart
